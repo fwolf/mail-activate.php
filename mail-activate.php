46205405492749cff948251ffe99d39ef04ec12c
Fwolf <fwolf.aide+git@gmail.com>
2014-01-22 17:06:59 +0800

    Retab file

--- mail-activate.php
+++ mail-activate.php
@@ -3,11 +3,11 @@
 /**
  * Active all my mailbox by send a boring mail to it self
  *
- * @package		fwolfbin
- * @copyright	Copyright 2007-2008, Fwolf
- * @author		Fwolf <fwolf.aide+fwolfbin@gmail.com>
- * @since		2007-03-30
- * @version		$Id$
+ * @package     fwolfbin
+ * @copyright   Copyright 2007-2008, Fwolf
+ * @author      Fwolf <fwolf.aide+fwolfbin@gmail.com>
+ * @since       2007-03-30
+ * @version     $Id$
  */
 
 define('P2R', dirname(__FILE__) . '/');
@@ -30,29 +30,29 @@ $mailer = new Mailsender;
 //$mailer->SetTo($to);
 foreach ($mail as $host)
 {
-	$mailer->SetHost($host['host'], $host['port']);
-	foreach ($host['mail'] as $account)
-	{
-		// Active also mail to self
-		$mailer->SetTo($to . ';' . $account['name']);
-		$mailer->SetFrom($account['name']);
-		$mailer->SetAuth($account['user'], $account['pass']);
+    $mailer->SetHost($host['host'], $host['port']);
+    foreach ($host['mail'] as $account)
+    {
+        // Active also mail to self
+        $mailer->SetTo($to . ';' . $account['name']);
+        $mailer->SetFrom($account['name']);
+        $mailer->SetAuth($account['user'], $account['pass']);
 
-		// Subject
-		$mailer->SetSubject('Msg from ' . $account['name'] . ' .');
+        // Subject
+        $mailer->SetSubject('Msg from ' . $account['name'] . ' .');
 
-		// Get random mail body
-		$b = shell_exec('/home/fwolf/.mutt/signature');
-		$b = substr($b, strpos($b, "\n"));
-		$mailer->SetBody($b);
+        // Get random mail body
+        $b = shell_exec('/home/fwolf/.mutt/signature');
+        $b = substr($b, strpos($b, "\n"));
+        $mailer->SetBody($b);
 
-		$rs = $mailer->Send();
-		if (false == $rs)
-			echo $mailer->mErrorMsg . "\n";
-		$rs = ($rs) ? 'Successful' : 'Failed';
-		$date = date('Y-m-d H:i:s');
-		echo("[$date] $rs active mail for " . $account['name'] . " !\n");
-		sleep(3);
-	}
+        $rs = $mailer->Send();
+        if (false == $rs)
+            echo $mailer->mErrorMsg . "\n";
+        $rs = ($rs) ? 'Successful' : 'Failed';
+        $date = date('Y-m-d H:i:s');
+        echo("[$date] $rs active mail for " . $account['name'] . " !\n");
+        sleep(3);
+    }
 }
 ?>
